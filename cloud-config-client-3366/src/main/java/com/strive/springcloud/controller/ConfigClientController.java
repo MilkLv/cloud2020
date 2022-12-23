e123bbb4ba8cb0c0edf458f30e90c2526a1a36b3
MilkLv <lvzhiping1994@163.com>
2022-12-23 16:36:20 +0800

    rabbit

---
+++ cloud-config-client-3366/src/main/java/com/strive/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.strive.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author l moonlight
+ * @create 2022-12-23 1:51
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return "serverPort:"+serverPort+"\t\n\n configInfo:"+configInfo;
+    }
+}
