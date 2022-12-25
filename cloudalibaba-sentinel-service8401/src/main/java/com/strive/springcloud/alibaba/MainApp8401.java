431ba511c548ecc90c5f97a9a55a651e1ca218a2
MilkLv <lvzhiping1994@163.com>
2022-12-25 22:15:24 +0800

    spring cloud alibaba sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/strive/springcloud/alibaba/MainApp8401.java
@@ -0,0 +1,17 @@
+package com.strive.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author l moonlight
+ * @create 2022-12-25 19:43
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/strive/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,59 @@
+package com.strive.springcloud.alibaba.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author l moonlight
+ * @create 2022-12-25 19:44
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
+        return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        //暂停几秒钟线程
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int a = 10/0;
+        return "------testD";
+    }
+
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        int age = 10 / 0 ;
+        return ".....testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "...deal_testHotKey";
+    }
+}
